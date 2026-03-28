53c72b2b134c05f1dc961c3f1ebd45e1e6ef9fcb
SafaeElouatyq <safaeelouatyq@gmail.com>
2026-03-28 16:26:11 +0100

    add forgot password part

--- Backend/src/routes/authRoutes.js
+++ Backend/src/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { register,verifyCode,login,resendCode, googleLogin } from "../controllers/authController.js";
+import { register,verifyCode,login,resendCode, googleLogin,forgotPassword,resetPassword } from "../controllers/authController.js";
 
 const router = express.Router();
 
@@ -8,5 +8,8 @@ router.post("/verify-code", verifyCode);
 router.post("/login",login);
 router.post("/resend-code",resendCode);
 router.post("/google-login",googleLogin);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password", resetPassword);
+
 
 export default router;
--- Backend/src/services/emailService.js
+++ Backend/src/services/emailService.js
@@ -23,3 +23,19 @@ export const sendVerificationCode = async (to,code)=>{
     })
 
 }
+
+
+export const  sendResetPassword =async (to,code)=>{
+    await transporter.sendMail({
+        from: process.env.EMAIL_USER,
+        to,
+        subject: "Votre code de reinitilisation du mot de passe",
+        html: `
+      <h2>Reset Password</h2>
+      <p>Votre code de reinitialisation est :</p>
+      <h1>${code}</h1>
+      <p>Ce code expire dans 10 minutes.</p>
+    `,
+    })
+
+} 
